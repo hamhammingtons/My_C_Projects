f26bc5eade14244a75b383c7e46d862e8c68efab
alex <cgazelka@gmail.com>
2026-02-13 17:24:13 +0500

    just done this for now

---
+++ general_learning/the_SCARY_stuff/functions/idk_testing.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+// int add(int x, int y);
+
+// int main()
+// {
+//     int result = add(5, 3);
+//     printf("%d", result);
+//     return 0;
+// }
+
+// int add(int x, int y)
+// {
+//     return x + y;
+// }
+
+// // very weird format of doing this, but its better i think
+
+int max_num(int *arr, int length);
+
+int main()
+{
+    int array[] = {2, 3, 141, 1458};
+    int length_one = sizeof(array) / sizeof(array[0]);
+
+    int res = max_num(array, length_one);
+}
+
+int max_num(int *arr, int length)
+{
+    i
+}
