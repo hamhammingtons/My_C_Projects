eb0ed9becdc68146239c82f3a9ad9928397e9ee2
alex <cgazelka@gmail.com>
2026-02-15 18:25:55 +0500

    done this finally

--- general_learning/the_SCARY_stuff/memory_and_pointers_stuff/Summary.c
+++ general_learning/the_SCARY_stuff/memory_and_pointers_stuff/Summary.c
@@ -4,17 +4,79 @@ theyre are physical slots for example when you need to change a variable
 but i am stupid a bit i need to revise them every time
 thats why im writing this
 */
-void change_val(int *arr);
-void call_func(void (*function_name)(char[]));
+#include <stdio.h>
+//
+int add(int x, int y) { return x + y; }
+int divide(int x, int y) { return x / y; } // wrong but it has to return INT
+//
 
-void add(int x, int y) { return x + y; }
-void divide(int x, int y) { return (float)x / y; }
+void change_val(int *arr, int index, int value);
 
-int list[] = {2,
-              3,
-              4};
+void call_func(void (*function_name)());
 
-int main()
+int calculator(int x, int y, int OPER_ID); // oper id is pretty cool look at the bottom
+
+void template_function() { printf("Hi from template.\n"); };
+
+int list[] = {
+    2,
+    3,
+    4,
+};
+
+int main() // fix this plz i forgot
 {
-    d
+    change_val(list, 2, 1248);
+    call_func(template_function);
+    calculator(1, 2, 0); // feels like magic numbers but uhh cant do nothing
+}
+
+void change_val(int *arr, int index, int value)
+{
+    arr[index] = value;
+    printf("Value changed. \n");
+    for (int i = 0; i < 3; i++) // bad practice ask user for a size plz
+    {
+        printf("%d\n", arr[i]);
+    }
+}
+
+// we use * in this case to create a pointer to the original array so we can replace data
+// we DONT use *arr[index] = value because we already have got a pointer, which can point to data.
+// also this arr[index] is like a slient *(arr + index)
+
+// CALL_FUNC METHOD
+//
+void call_func(void (*function_name)())
+{
+    function_name();
+    printf("Code above is executed by (address) %p\n ", (void *)function_name);
+    // (void *)function_name isnt really that important, its just so %p can work correctly. Returns hex.
+}
+
+// CALCULATOR
+
+int calculator(int x, int y, int OPER_ID)
+{
+    int (*OPER_ALLOWED[2])(int, int) = {add, divide}; // jump table
+    /*
+    so its basically like this format:
+
+    name,   role,       adress,     content
+    add,    func,       0x100,      binary num to do add
+    oper[0] array slot, 0x00,       0x100 (pointer to add)
+
+    */
+    // int D = OPER_ALLOWED[0](1, 2);
+    // printf("WRN: %d", D);
+
+    if (OPER_ID >= 0 && OPER_ID < 2)
+    {
+        return OPER_ALLOWED[OPER_ID](x, y);
+    }
+    else
+    {
+        printf("incorrect type.\n");
+        return -1;
+    }
 }
