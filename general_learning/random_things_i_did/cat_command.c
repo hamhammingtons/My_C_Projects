6566813f8155f9d3675cbbcb0bf2340d7530b70e
alex <cgazelka@gmail.com>
2026-03-04 15:05:23 +0500

    i mean it works soo

--- general_learning/random_things_i_did/cat_command.c
+++ general_learning/random_things_i_did/cat_command.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-enum FLAG_STR_TO_NUM
+
+typedef enum n
 {
+    NONE_FLAG = 0,
     NUMERATE_M = 1
-};
+} FLAG_STR_TO_NUM;
+
 typedef struct command_check_fill_roadmap // we use * to mention that it is a string
 {
     char *comm_name;
-    char flag;
+    char *flag;
     char *arg;
 } command_t;
 
+void cat(command_t *user_input);
+FLAG_STR_TO_NUM handle_flag_to_num(char flag[3]);
+
 typedef void (*permitted_commands)(command_t *);
+
+permitted_commands allowed_functions[] = {cat};
+
+int main()
+{
+    command_t cmd = {"cat", "-n", "fat.txt"};
+    allowed_functions[0](&cmd);
+    return 0;
+}
 
 void cat(command_t *user_input)
 {
+    FLAG_STR_TO_NUM flag_numeric = handle_flag_to_num(user_input->flag); // fetch for switch statement
+
     if (!user_input->arg)
         return;
 
@@ -25,6 +42,7 @@ void cat(command_t *user_input)
         perror("Error");
         return;
     }
+    // memory stuff
 
     fseek(f_pointer, 0, SEEK_END);
     long sizeof_file = ftell(f_pointer); // tells us bytes
@@ -39,31 +57,49 @@ void cat(command_t *user_input)
     }
     size_t read_size = fread(content, 1, sizeof_file, f_pointer);
     content[read_size] = '\0';
+    // memory stuff end
 
-    switch (user_input->flag)
+    // actual logic below
+
+    switch (flag_numeric)
     {
-    case NUMERATE_M: // TODO: do this
-        /* code */
+    case NUMERATE_M:
+        char current_line = 1;
+        char is_start_of_line = 1;
+
+        for (int i = 0; i < read_size; i++)
+        {
+            if (is_start_of_line)
+            {
+                printf("%6d ", current_line++);
+                is_start_of_line = 0;
+            }
+            putchar(content[i]);
+
+            if (content[i] == '\n') // dont forget it must be a '\n' because its a char not a "\n"
+            {
+                is_start_of_line = 1;
+            }
+        }
+
         break;
 
     default:
+        printf("%s", content);
         break;
     }
-    printf("%s", content);
 
     free(content);
     fclose(f_pointer);
     f_pointer = NULL;
 }
 
-permitted_commands allowed_functions[] = {cat};
-
-int main()
+FLAG_STR_TO_NUM handle_flag_to_num(char *flag)
 {
-    command_t cmd = {"cat", "default", "fat.txt"};
-    allowed_functions[0](&cmd);
-    return 0;
+    if (flag == NULL)
+        return NONE_FLAG;
+    else if (strcmp(flag, "-n") == 0) // 0 is True if identical
+    {
+        return NUMERATE_M;
+    }
 }
-
-// NOTE: re-learn file handling again
-/*https://www.youtube.com/watch?v=kJ70tXVYnws&list=PLfVsf4Bjg79BOmLYBRTwqCIkGPiOWb7xj*/
